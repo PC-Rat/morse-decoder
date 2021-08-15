65378e7988b952179a50808d69696d4ed5f6a254
PC-Rat <32428513+PC-Rat@users.noreply.github.com>
2021-08-15 13:01:57 +0300

    init: task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,37 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    	const morseBinarArr = expr.split('').join('');
+
+	if (expr.length % 10 !== 0) {
+		return false;
+		const morseSubstr = '';
+	}
+	for (let i = 0; i < morseBinarArr.length; i + 10) {
+		morseSubstr = morseBinarArr.substr(i, 10);
+
+		if (morseSubstr[i].includes('**********')) {
+			return (`$ {' '}`);
+		}
+		let dot = '.';
+		let dash = '-';
+		if (morseSubstr[i].includes('11')) {
+			return (`${dash}`);
+		}
+		if (morseSubstr[i].includes('10')) {
+			return (`$ {
+					dot
+				}`);
+		}
+		if (morseSubstr[i].includes('00')) {
+			return (`$ {
+						''
+					}`);
+		}
+		let morseDecode = morseSubstr.includes(MORSE_TABLE.key);
+		morseDecode = MORSE_TABLE.value;
+	}
+	return morseDecode;
 }
 
 module.exports = {
